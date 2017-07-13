a603f229745a78720e851e638b32f68f956a6098
ss <352070351@qq.com>
2017-07-13 17:12:10 +0800

    add test

--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -30,4 +30,292 @@ describe('pos', () => {
 
     expect(console.log).toHaveBeenCalledWith(expectText);
   });
+
+
+  //test function buildObject();
+  it('when tags come in, should return Object', () => {
+
+      const tags = [
+          'ITEM000001',
+          'ITEM000001',
+          'ITEM000001',
+          'ITEM000001',
+          'ITEM000001',
+          'ITEM000003-2.5',
+          'ITEM000005',
+          'ITEM000005-2',
+      ];
+
+      const result = [
+          {
+              barcode: 'ITEM000001',
+              name: '雪碧',
+              unit: '瓶',
+              price: 3.00,
+              count: 1.00
+          },
+          {
+              barcode: 'ITEM000001',
+              name: '雪碧',
+              unit: '瓶',
+              price: 3.00,
+              count: 1.00
+          },
+          {
+              barcode: 'ITEM000001',
+              name: '雪碧',
+              unit: '瓶',
+              price: 3.00,
+              count: 1.00
+          },
+          {
+              barcode: 'ITEM000001',
+              name: '雪碧',
+              unit: '瓶',
+              price: 3.00,
+              count: 1.00
+          },
+          {
+              barcode: 'ITEM000001',
+              name: '雪碧',
+              unit: '瓶',
+              price: 3.00,
+              count: 1.00
+          },
+          {
+              barcode: 'ITEM000003',
+              name: '荔枝',
+              unit: '斤',
+              price: 15.00,
+              count: 2.5
+          },
+          {
+              barcode: 'ITEM000005',
+              name: '方便面',
+              unit: '袋',
+              price: 4.50,
+              count: 1.0
+          },
+          {
+              barcode: 'ITEM000005',
+              name: '方便面',
+              unit: '袋',
+              price: 4.50,
+              count: 2.0
+          }
+      ];
+
+      expect(buildObject(tags)).toEqual(result);
+  })  ;
+
+
+  //test function rmSameObject()
+    it('should return a object set', () => {
+
+        const inputs =[
+            {
+                barcode: 'ITEM000001',
+                name: '雪碧',
+                unit: '瓶',
+                price: 3.00,
+                count: 1.00
+            },
+            {
+                barcode: 'ITEM000001',
+                name: '雪碧',
+                unit: '瓶',
+                price: 3.00,
+                count: 1.00
+            },
+            {
+                barcode: 'ITEM000001',
+                name: '雪碧',
+                unit: '瓶',
+                price: 3.00,
+                count: 1.00
+            },
+            {
+                barcode: 'ITEM000001',
+                name: '雪碧',
+                unit: '瓶',
+                price: 3.00,
+                count: 1.00
+            },
+            {
+                barcode: 'ITEM000001',
+                name: '雪碧',
+                unit: '瓶',
+                price: 3.00,
+                count: 1.00
+            },
+            {
+                barcode: 'ITEM000003',
+                name: '荔枝',
+                unit: '斤',
+                price: 15.00,
+                count: 2.5
+            },
+            {
+                barcode: 'ITEM000005',
+                name: '方便面',
+                unit: '袋',
+                price: 4.50,
+                count: 1.0
+            },
+            {
+                barcode: 'ITEM000005',
+                name: '方便面',
+                unit: '袋',
+                price: 4.50,
+                count: 2.0
+            }
+        ];
+
+        const result = [
+            {
+                barcode: 'ITEM000001',
+                name: '雪碧',
+                unit: '瓶',
+                price: 3.00,
+                count: 5
+            },
+            {
+                barcode: 'ITEM000003',
+                name: '荔枝',
+                unit: '斤',
+                price: 15.00,
+                count: 2.5
+            },
+            {
+                barcode: 'ITEM000005',
+                name: '方便面',
+                unit: '袋',
+                price: 4.50,
+                count: 3
+            }
+        ];
+
+        expect(rmSameObject(inputs)).toEqual(result);
+    });
+
+    //test function finalObject()
+    it('should return the final object array', () => {
+
+        const inputs = [
+            {
+                barcode: 'ITEM000001',
+                name: '雪碧',
+                unit: '瓶',
+                price: 3.00,
+                count: 5
+            },
+            {
+                barcode: 'ITEM000003',
+                name: '荔枝',
+                unit: '斤',
+                price: 15.00,
+                count: 2.5
+            },
+            {
+                barcode: 'ITEM000005',
+                name: '方便面',
+                unit: '袋',
+                price: 4.50,
+                count: 3
+            }
+        ];
+
+        const result = [
+            {
+                barcode: 'ITEM000001',
+                name: '雪碧',
+                unit: '瓶',
+                price: 3.00,
+                count: 5,
+                sum:12
+            },
+            {
+                barcode: 'ITEM000003',
+                name: '荔枝',
+                unit: '斤',
+                price: 15.00,
+                count: 2.5,
+                sum:37.5
+            },
+            {
+                barcode: 'ITEM000005',
+                name: '方便面',
+                unit: '袋',
+                price: 4.50,
+                count: 3,
+                sum:9
+            }
+        ];
+        totalsave = 0;
+        totalsum = 0;
+
+        expect(finalObject(inputs)).toEqual(result);
+    });
+
+    //test function buildString()
+    it('should return string array', () => {
+
+        const inputs = [
+            {
+                barcode: 'ITEM000001',
+                name: '雪碧',
+                unit: '瓶',
+                price: 3.00,
+                count: 5,
+                sum:12
+            },
+            {
+                barcode: 'ITEM000003',
+                name: '荔枝',
+                unit: '斤',
+                price: 15.00,
+                count: 2.5,
+                sum:37.5
+            },
+            {
+                barcode: 'ITEM000005',
+                name: '方便面',
+                unit: '袋',
+                price: 4.50,
+                count: 3,
+                sum:9
+            }
+        ];
+
+        const result = [
+            `名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)`,
+            `名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)`,
+            `名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)`
+        ];
+
+        expect(buildString(inputs)).toEqual(result);
+    });
+
+
+    //test function print();
+    it('should print receipt', () => {
+
+        const input = [
+            `名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)`,
+            `名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)`,
+            `名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)`
+        ];
+
+        const result = `***<没钱赚商店>收据***
+名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+----------------------
+总计：58.50(元)
+节省：7.50(元)
+**********************`;
+        expect(print(input)).toEqual(result);
+    });
+
+
 });
